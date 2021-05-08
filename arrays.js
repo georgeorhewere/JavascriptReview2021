9391572df12200e666ed4d03cabd9c11f158c330
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-08 19:38:31 +0100

    organized array review by method

--- arrays.js
+++ arrays.js
@@ -2,18 +2,33 @@ function testArrays(){
 
 var a = [12,2,8];
 var b = [5,14,7];
+//concat
 var temp = a.concat(b);
 print(temp);
+// join
 var d = a.join('');
 print(d);
+//stack operations
+//push
 var t = a.push(34);
-var sortedTemp = a.sort((a,b)=>{ return a - b});
-var ans =a.find(x=> x==2);
-print({type:"find",ans})
-print(t)
+print({type:"After Push",t})
 print(a)
+var arrPop = a.pop();
+print({type:"popped val",arrPop});
+print(a)
+//end stack operations
+
+// sort numbers
+var sortedTemp = a.sort((a,b)=>{ return a - b});
 print("sorted")
 print(sortedTemp)
+
+// find
+var ans =a.find(x=> x==2);
+print({type:"find "+ 2,ans})
+
+
+
 
 }
 
@@ -21,5 +36,5 @@ function print(value){
     console.log(value);
 }
 
-module.exports ={ testArrays, print };
+module.exports ={ testArrays };
 
