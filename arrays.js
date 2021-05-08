565504d59c9e3e55d027a5023ef1d2242924dfcf
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-08 20:06:47 +0100

    concepts pracitce

--- arrays.js
+++ arrays.js
@@ -4,34 +4,47 @@ var a = [12,2,8];
 var b = [5,14,7];
 //concat
 var temp = a.concat(b);
-print(temp);
+// print(temp);
 // join
 var d = a.join('');
-print(d);
+// print(d);
 //stack operations
 //push
 var t = a.push(34);
-print({type:"After Push",t})
-print(a)
+// print({type:"After Push",t})
+// print(a)
 var arrPop = a.pop();
-print({type:"popped val",arrPop});
-print(a)
+// print({type:"popped val",arrPop});
+// print(a)
 //end stack operations
 
 // sort numbers
 var sortedTemp = a.sort((a,b)=>{ return a - b});
-print("sorted")
-print(sortedTemp)
+// print("sorted")
+// print(sortedTemp)
 
 // find
 var ans =a.find(x=> x==2);
-print({type:"find "+ 2,ans})
+// print({type:"find "+ 2,ans})
 
-// aggregator function .. operates on al items in the array
+// aggregator function .. operates on all items in the array
 var res = a.reduce((t,x) =>{ return t+=x } );
-print(res);
+//recursion
+var res2 = factorial(5);
 
+ print(res2);
+// 
+var word1 = "stressredse";
+var stringArr = word1.split('');
 
+print(stringArr);
+
+}
+
+function factorial(num){
+    if(num <=1)
+        return 1;
+    return num * factorial(num -1);
 }
 
 function print(value){
