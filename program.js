9725c11a9e5586c5cc693ad0519858923b59242c
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-08 18:11:43 +0100

    use multiple files in javascript review

--- program.js
+++ program.js
@@ -8,6 +8,8 @@ var d = a.join('');
 print(d);
 var t = a.push(34);
 var sortedTemp = a.sort((a,b)=>{ return a - b});
+var ans =a.find(x=> x==2);
+print({type:"find",ans})
 print(t)
 print(a)
 print("sorted")
