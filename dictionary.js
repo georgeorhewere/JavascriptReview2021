e97b17068d5e0d1393b1d255371f653cb3347615
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-09 15:04:21 +0100

    correct count of duplicate items. need to reduce the instance array

--- dictionary.js
+++ dictionary.js
@@ -3,15 +3,17 @@ function countDuplicates(word){
     var characterInstances = new Array();
     var wordArray = word.split('');
     
-    wordArray.forEach(element => {
-        console.log(characterInstances[element]);
-        if(characterInstances[element]){
-            console.log("increase count "+ element );
+    wordArray.forEach(element => {        
+
+        if(characterInstances[element] != null)
+        {            
             characterInstances[element]++;
         }else{
             characterInstances[element] = 0;
         }
     });
+
+    
 
     console.log(wordArray, characterInstances);
 
