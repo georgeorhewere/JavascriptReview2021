8f2f328aef0057ee27eca014a05e770a8f40c000
george.orhewere@gmail.com <george.orhewere@gmail.com>
2021-05-09 15:35:01 +0100

    optimized version of count duplicates in string

--- dictionary.js
+++ dictionary.js
@@ -3,24 +3,27 @@ function countDuplicates(word){
     
     var characterInstances =Object.create({});
     var wordArray = word.split('');
+    var duplicates = 0;
     
     wordArray.forEach(element => {        
 
         if(characterInstances[element] != null)
         {            
-            characterInstances[element]++;
+            duplicates++;
         }else{
             characterInstances[element] = 0;
         }
     });
 
-    var totalDuplicates =  Object.values(characterInstances).filter(x => x > 0)
-                                                         .reduce((t,c) =>{                                                             
-                                                             return t += c
-                                                         });
-    
-    console.log("total : "+ totalDuplicates);
+    // would break if no duplicates exists and requires the addition of a if statement
+    // would be useful is count has some relationship with the character
 
+    // var totalDuplicates =  Object.values(characterInstances).filter(x => x > 0)
+    //                                                      .reduce((t,c) =>{                                                             
+    //                                                          return t += c
+    //                                                      });
+    
+    console.log("total : "+ duplicates);
 
 }
 
--- program.js
+++ program.js
@@ -4,5 +4,9 @@ let tests = require('./dictionary');
 console.log("Hello");
 
 // arr.testArrays();
+// test 1
 var word1 = "baabbccdef";
+tests.countDuplicates(word1);
+// test 2
+word1 = "abcdef";
 tests.countDuplicates(word1);
